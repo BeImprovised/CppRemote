05a08e3220cf3f64886deb1aac5ddc6f94af00cf
beunprepared <beunprepared@gmail.com>
2015-07-15 02:07:49 -0500

    compile parameter -pthread added to cmake

---
+++ examples/example6/build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/_home/beunprepared/working/RPC/CppRemote/examples/example6/src/remote_reader.cpp" "/_home/beunprepared/working/RPC/CppRemote/examples/example6/build/CMakeFiles/server.dir/src/remote_reader.cpp.o"
+  "/_home/beunprepared/working/RPC/CppRemote/examples/example6/src/remote_topic.cpp" "/_home/beunprepared/working/RPC/CppRemote/examples/example6/build/CMakeFiles/server.dir/src/remote_topic.cpp.o"
+  "/_home/beunprepared/working/RPC/CppRemote/examples/example6/src/server.cpp" "/_home/beunprepared/working/RPC/CppRemote/examples/example6/build/CMakeFiles/server.dir/src/server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../../include"
+  "../src"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ examples/example6/build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/src/remote_reader.cpp.o"
+  "CMakeFiles/server.dir/src/remote_topic.cpp.o"
+  "CMakeFiles/server.dir/src/server.cpp.o"
+  "../bin/server.pdb"
+  "../bin/server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
